077b4577eb31f64a072e35efa0617b059475833d
Carlos Eduardo <carloseduardobr04@gmail.com>
2023-12-12 00:21:57 -0300

    mais coisas

---
+++ Inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,19 @@
+//Bloco #01
+{
+    console.log("1")
+    console.log("3")
+    console.log("5")
+    console.log("7")
+    console.log("9")
+
+}
+
+//Bloco #02
+{
+    console.log("2")
+    console.log("4")
+    console.log("6")
+    console.log("8")
+    console.log("10")
+
+}
